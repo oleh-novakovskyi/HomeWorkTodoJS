c40ca1dd89849f41f41b2c9669033989f348c49b
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-02-18 22:02:57 +0200

    #2

---
+++ #2/main.js
@@ -0,0 +1,36 @@
+'use strict'
+
+//Task #1 
+/*
+const one = Number(prompt('Enter the first digit!')); //получаем первое число
+const two = Number(prompt('Enter the second digit!')); //получаем второе число
+const sum = one + two; //сложение двух чисел
+alert(sum); // вывод сумма в alert
+console.log(sum); // вывод суммы в конcоль
+*/
+
+//Task #2
+/*
+const userName = prompt('What is your name?'); //получаем Имя
+alert("Hello, " + userName + "!"); // вывод Hello, userName в alert
+console.log("Hello, " + userName + "!"); // вывод userName в конcоль
+*/
+
+//Task #3
+/*
+const numeric = Number(prompt('What do we divide by 2?')); //получаем число
+const sum = numeric % 2; //диление numeric на 2 
+alert(sum); // вывод sum в alert
+console.log(sum); // вывод sum в конcоль
+*/
+
+//Task #4
+/*
+const numeric = Number(prompt('Enter the numeric digit!')); //получаем число
+const before = Math.floor(numeric); //забираем первое число 
+const after = numeric.toString().split(".")[1]; // тут не уверен что правильный подход (забираем то, что после точки)
+alert(before); //вывод результата в alert
+alert("0." + after); //(скорее всего колхоз) вывод результата в alert
+console.log(before); // вывод результата в консоль
+console.log("0." + after); //вывод результата в консоль
+*/
